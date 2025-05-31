99e7c01cf872e99c7707a0d09443982f0eae30e0
Dante Lopez <lordran@lordran-2.local>
2025-05-31 16:26:04 -0600

    Init fresh project

---
+++ README.md
@@ -0,0 +1,3 @@
+# PySockets
+
+This repo has the purpose of building a socket manager daemon which can be controlled by CLI
--- src/message.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from typing import TypedDict
-
-
-class Message(TypedDict):
-    timestamp: datetime
-    source_port: int
-    client_address: str
-    message: str
