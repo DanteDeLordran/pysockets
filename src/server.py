da70f9d3f83a6c51d17942f31e4d61424cded367
Dante Lopez <lordran@lordran-2.local>
2025-06-18 16:53:43 -0600

    Placeholder server socket

---
+++ src/server.py
@@ -0,0 +1,59 @@
+import socket
+
+from _typeshed import ReadableBuffer
+import threading
+
+HOST = "0.0.0.0"
+PORT = 60_000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+clients: list[socket.socket] = []
+nicknames = []
+
+
+def broadcast(message: ReadableBuffer) -> None:
+    for client in clients:
+        client.send(message)
+
+
+def handle(client: socket.socket) -> None:
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except Exception as e:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+
+            broadcast(f"{nickname} had error {e}".encode("ascii"))
+            broadcast(f"{nickname} left the chat".encode("ascii"))
+
+            nicknames.remove(nickname)
+            break
+
+
+def recieve() -> None:
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {address}")
+
+        client.send("Nick".encode("ascii"))
+        nickname = client.recv(1024).decode("ascii")
+        nicknames.append(nickname)
+
+        print(f"Nickname of the client is : {nickname}")
+        broadcast(f"{nickname} joined the chat".encode("ascii"))
+
+        client.send("Connected to the server".encode("ascii"))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("Server is listening...")
+recieve()
