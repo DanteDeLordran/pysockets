414752e6b53ecd6fc7c546b0cf4b251e1cd0cb24
Dante Lopez <lordran@lordran-2.local>
2025-06-18 20:20:00 -0600

    Using classes for code isolating and reusability

--- src/client.py
+++ src/client.py
@@ -1,37 +1,37 @@
 import socket
 import threading
 
-HOST = "0.0.0.0"
-PORT = 60_000
 
-nickname = input("Nickname: ")
+class Client:
+    def __init__(self, host: str, port: int, nickname: str):
+        self.host = host
+        self.port = port
+        self.nickname = nickname
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+    def recieve(self):
+        while True:
+            try:
+                message = self.client.recv(1024).decode("ascii")
+                if message == "NICK":
+                    self.client.send(self.nickname.encode("ascii"))
+                else:
+                    print(message)
+            except Exception as e:
+                print(f"Error ocurred due to: {e}")
+                self.client.close()
+                break
 
+    def write(self):
+        while True:
+            message = f"{self.nickname}: {input('')}"
+            self.client.send(message.encode("ascii"))
 
-def recieve():
-    while True:
-        try:
-            message = client.recv(1024).decode("ascii")
-            if message == "NICK":
-                client.send(nickname.encode("ascii"))
-            else:
-                print(message)
-        except Exception as e:
-            print(f"Error ocurred due to: {e}")
-            client.close()
-            break
+    def start(self):
+        self.client.connect((self.host, self.port))
 
+        recieve = threading.Thread(target=self.recieve)
+        recieve.start()
 
-def write():
-    while True:
-        message = f"{nickname}: {input('')}"
-        client.send(message.encode("ascii"))
-
-
-recieve_thread = threading.Thread(target=recieve)
-recieve_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
+        write = threading.Thread(target=self.write)
+        write.start()
--- src/server.py
+++ src/server.py
@@ -1,57 +1,55 @@
 import socket
 import threading
 
-HOST = "0.0.0.0"
-PORT = 60_000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen()
+class Server:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients: list[socket.socket] = []
+        self.nicknames: list[str] = []
 
-clients: list[socket.socket] = []
-nicknames = []
+    def broadcast(self, message: bytes) -> None:
+        for client in self.clients:
+            client.send(message)
 
+    def handle(self, client: socket.socket) -> None:
+        while True:
+            try:
+                message = client.recv(1024)
+                self.broadcast(message)
+            except Exception as e:
+                index = self.clients.index(client)
+                self.clients.remove(client)
+                client.close()
+                nickname = self.nicknames[index]
+                self.nicknames.remove(nickname)
 
-def broadcast(message: bytes) -> None:
-    for client in clients:
-        client.send(message)
+                self.broadcast(f"{nickname} had error {e}".encode("ascii"))
+                self.broadcast(f"{nickname} left the chat".encode("ascii"))
 
+                break
 
-def handle(client: socket.socket) -> None:
-    while True:
-        try:
-            message = client.recv(1024)
-            broadcast(message)
-        except Exception as e:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
+    def recieve(self) -> None:
+        while True:
+            client, address = self.server.accept()
+            print(f"Connected with {address}")
 
-            broadcast(f"{nickname} had error {e}".encode("ascii"))
-            broadcast(f"{nickname} left the chat".encode("ascii"))
+            client.send("NICK".encode("ascii"))
+            nickname = client.recv(1024).decode("ascii")
+            self.nicknames.append(nickname)
+            self.clients.append(client)
 
-            nicknames.remove(nickname)
-            break
+            print(f"Nickname of the client is : {nickname}")
+            self.broadcast(f"{nickname} joined the chat".encode("ascii"))
+            client.send("Connected to the server".encode("ascii"))
 
+            thread = threading.Thread(target=self.handle, args=(client,))
+            thread.start()
 
-def recieve() -> None:
-    while True:
-        client, address = server.accept()
-        print(f"Connected with {address}")
-
-        client.send("NICK".encode("ascii"))
-        nickname = client.recv(1024).decode("ascii")
-        nicknames.append(nickname)
-        clients.append(client)
-
-        print(f"Nickname of the client is : {nickname}")
-        broadcast(f"{nickname} joined the chat".encode("ascii"))
-        client.send("Connected to the server".encode("ascii"))
-
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
-
-
-print("Server is listening...")
-recieve()
+    def start(self):
+        self.server.bind((self.host, self.port))
+        self.server.listen()
+        print(f"Server is listening on port: {self.port}")
+        self.recieve()
